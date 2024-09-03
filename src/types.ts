9f8d47888d99938a8ea5f97d23c0122dfd1f8b05
minttuNB <cerealley@gmail.com>
2024-09-03 19:42:58 +0200

    Nullable props for proj object type

--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,10 @@ import { UUID } from "crypto";
 type ProjectObject = {
 	id: UUID;
 	name: string;
-	description: string;
-	date: Date;
-	url: URL;
-	images: string[];
+	description?: string;
+	date?: Date;
+	url?: URL;
+	images?: URL[];
 };
 export type {
 	ProjectObject
