2c0999372a366d0580a57c0e3a0027e4a595b98b
minttuNB <cerealley@gmail.com>
2024-09-03 16:26:24 +0200

    Logic handling for the creation page

---
+++ src/create-project.ts
@@ -0,0 +1,35 @@
+import "./style.css";
+document.addEventListener("DOMContentLoaded", async () => {
+	const formDiv: HTMLFormElement =
+		document.querySelector<HTMLFormElement>("form")!;
+	formDiv.addEventListener("submit", async (e: SubmitEvent) => {
+		e.preventDefault();
+		let data = fetch("http://localhost:8080/api/projects", {
+			headers: {
+				"Content-Type": "application/json",
+			},
+			method: "PUT",
+			body: JSON.stringify(
+				Object.fromEntries(new FormData(formDiv).entries())
+			),
+		});
+		const submitButton: HTMLInputElement =
+			document.querySelector<HTMLInputElement>("input[type=submit]")!;
+		submitButton.disabled = true;
+		const mainElement: HTMLElement =
+			document.querySelector<HTMLElement>("main")!;
+		if ((await data).status === 201) {
+			mainElement.innerHTML =
+				"<h1>Success!</h1><p>You will be redirected to the projects page shortly.</p>";
+			window.location.replace(`${window.location.origin}`);
+		} else {
+			document
+				.querySelector<HTMLParagraphElement>("#response-message")!
+				.classList.add("error-message");
+			document.querySelector<HTMLParagraphElement>(
+				"#response-message"
+			)!.innerHTML = "An error has occurred";
+			submitButton.disabled = false;
+		}
+	});
+});
