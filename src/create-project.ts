a2b0888ddf552526adebbd21998827668e4db3e3
minttuNB <cerealley@gmail.com>
2024-09-04 09:20:22 +0200

    link css instead of import; default val for description display

--- src/create-project.ts
+++ src/create-project.ts
@@ -1,4 +1,3 @@
-import "./style.css";
 document.addEventListener("DOMContentLoaded", async () => {
 	const formDiv: HTMLFormElement =
 		document.querySelector<HTMLFormElement>("form")!;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
-import "./style.css";
 import { ProjectObject } from "./types.ts";
 document.addEventListener("DOMContentLoaded", async () => {
 	const projectDiv: HTMLDivElement =
 		document.querySelector<HTMLDivElement>("#projects")!;
 	let data: Promise<ProjectObject[]> = fetch("http://localhost:8080/api/projects").then((r) => r.json());
 	(await data).forEach((element: ProjectObject) => {
-		projectDiv.innerHTML += `<article><h2>${element.name}</h2>${element.description}</article>`;
+		projectDiv.innerHTML += `<article><h2>${element.name}</h2>${element.description || "<i>No description provided</i>"}</article>`;
 	});
 });
