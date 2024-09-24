d771beb7daa987a422369b0d177a45692bc1a7cc
minttuNB <cerealley@gmail.com>
2024-09-24 17:56:16 +0200

    Make projects use props with children

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -1,20 +1,26 @@
 import Project from "./Project";
 import { ProjectProps, ProjectsProps } from "../types";
+import { PropsWithChildren } from "react";
 
-export default function Projects(props: ProjectsProps) {
+export default function Projects(
+	props: Readonly<PropsWithChildren<ProjectsProps>>
+) {
 	return (
-		<section id="projects">
-			{props.projects.map((project: ProjectProps) => (
-				<Project
-					id={project.id}
-					name={project.name}
-					description={project.description}
-					date={project.date}
-					url={project.url}
-					images={project.images}
-					key={project.id}
-				/>
-			))}
-		</section>
+		<>
+			{props.children}
+			<section id="projects">
+				{props.projects.map((project: ProjectProps) => (
+					<Project
+						id={project.id}
+						name={project.name}
+						description={project.description}
+						date={project.date}
+						url={project.url}
+						images={project.images}
+						key={project.id}
+					/>
+				))}
+			</section>
+		</>
 	);
 }
