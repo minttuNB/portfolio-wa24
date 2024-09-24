1203372c670fad0007e35eeac511efa8b92bfda8
minttuNB <cerealley@gmail.com>
2024-09-24 16:49:34 +0200

    Replace messy ifs with ternaries in Project component

--- frontend/src/components/Project.tsx
+++ frontend/src/components/Project.tsx
@@ -1,24 +1,26 @@
 import { ProjectProps } from "../types";
 
 export default function Project(props: ProjectProps) {
-	let image;
-	let { description } = props;
-	if(!props.images){
-		image = <img src="https://via.placeholder.com/300" alt={props.name}></img>;
-	}
-	else{
-		image = <img src={props.images[0].toString()} alt={props.name}></img>;
-	}
-	if(!description){
-		description = "<i>No description provided</i>";
-	}
 	return (
 		<article>
-			{image}
+			{!props.images ? (
+				<img
+					src="https://via.placeholder.com/300"
+					alt={props.name}
+				></img>
+			) : (
+				<img src={props.images[0].toString()} alt={props.name}></img>
+			)}
 			<section>
 				<h2>{props.name}</h2>
-				<p>{description}</p>
-				<a className="link-button" href={props.url?.toString()}>Learn more</a>
+				{props.description ? (
+					<p>{props.description}</p>
+				) : (
+					<i>No description provided</i>
+				)}
+				<a className="link-button" href={props.url?.toString()}>
+					Learn more
+				</a>
 			</section>
 		</article>
 	);
