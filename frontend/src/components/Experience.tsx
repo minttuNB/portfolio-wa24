d82ca597cf9063adbfaab12ba42c7b9f7cc9e2fd
minttuNB <cerealley@gmail.com>
2024-09-24 19:23:14 +0200

    Some styling for experiences

--- frontend/src/components/Experience.tsx
+++ frontend/src/components/Experience.tsx
@@ -6,8 +6,8 @@ export default function Experience(props: ExperienceProps) {
 			<h2>{props.name}</h2>
 			<p>{props.description}</p>
 			{props.toDate
-				? (<p>{props.fromDate.toDateString()}-{props.toDate.toDateString()}</p>)
-				: (<p>from {props.fromDate.toDateString()}</p>)}
+				? (<p>📅 {props.fromDate.toDateString()}-{props.toDate.toDateString()}</p>)
+				: (<p>📅 from {props.fromDate.toDateString()}</p>)}
 		</article>
 	);
 }
--- frontend/src/components/Experiences.tsx
+++ frontend/src/components/Experiences.tsx
@@ -2,20 +2,24 @@ import { PropsWithChildren } from "react";
 import { ExperiencesProps } from "../types";
 import Experience from "./Experience";
 
-export default function Experiences(props: Readonly<PropsWithChildren<ExperiencesProps>>) {
+export default function Experiences(
+	props: Readonly<PropsWithChildren<ExperiencesProps>>
+) {
 	return (
-		<section id="experiences">
+		<>
 			{props.children}
-			{props.experiences.map((experience) => (
-				<Experience
-					id={experience.id}
-					name={experience.name}
-					description={experience.description}
-					fromDate={experience.fromDate}
-					toDate={experience.toDate}
-					key={experience.id}
-				/>
-			))}
-		</section>
+			<section id="experiences">
+				{props.experiences.map((experience) => (
+					<Experience
+						id={experience.id}
+						name={experience.name}
+						description={experience.description}
+						fromDate={experience.fromDate}
+						toDate={experience.toDate}
+						key={experience.id}
+					/>
+				))}
+			</section>
+		</>
 	);
 }
