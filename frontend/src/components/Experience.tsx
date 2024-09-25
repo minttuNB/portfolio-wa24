abc7320eb22231bed860a8fd9e55cb43da06aafd
minttuNB <cerealley@gmail.com>
2024-09-25 16:39:38 +0200

    Better dates display

--- frontend/src/components/Experience.tsx
+++ frontend/src/components/Experience.tsx
@@ -6,8 +6,8 @@ export default function Experience(props: ExperienceProps) {
 			<h2>{props.name}</h2>
 			<p>{props.description}</p>
 			{props.toDate
-				? (<p>📅 {props.fromDate.toDateString()}-{props.toDate.toDateString()}</p>)
-				: (<p>📅 from {props.fromDate.toDateString()}</p>)}
+				? (<p>📅 {props.fromDate.toISOString().split("T")[0]} — {props.toDate.toISOString().split("T")[0]}</p>)
+				: (<p>📅 from {props.fromDate.toISOString().split("T")[0]}</p>)}
 		</article>
 	);
 }
