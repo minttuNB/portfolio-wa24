be699e79fe711b6401b7d0ba60e1970b2b16ca9e
minttuNB <cerealley@gmail.com>
2024-09-25 19:21:06 +0200

    Expand on contact form

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -9,7 +9,14 @@ export default function Contact(
 	return (
 		<>
 			{props.children}
-			<button onClick={()=>alert(`My email address is ${props.email}`)}>Show contact info</button>
+			<button onClick={()=>alert(`My email address is ${props.email}`)}>Show email address</button>
+			<form>
+				<label htmlFor="contact-name">Your name (required)</label>
+				<input type="text" name="contact-name" id="contact-name" required/>
+				<label htmlFor="contact-name">Your message</label>
+				<textarea name="contact-message" id="contact-message" rows={20} required></textarea>
+				<input type="submit" value="Send message" />
+			</form>
 		</>
 	);
 }
