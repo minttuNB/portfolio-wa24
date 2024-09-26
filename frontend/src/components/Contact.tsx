9e7547f2862fa8eb522067f14450446cf184b496
minttuNB <cerealley@gmail.com>
2024-09-26 11:29:13 +0200

    Fix controlled input, add simple validation

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -14,12 +14,22 @@ export default function Contact(
 	function updateName(event: FormEvent<HTMLInputElement>) {
 		const input = event.target as HTMLInputElement | null;
 		if (!input) return;
+		if (input.value.length<1) {
+			setNameError("Your name must be at least 1 character long.");
+			return;
+		}
 		setName(input.value);
+		setNameError("");
 	}
 	function updateMessage(event: FormEvent<HTMLTextAreaElement>) {
 		const input = event.target as HTMLTextAreaElement | null;
 		if (!input) return;
+		if (input.value.length<1) {
+			setMessageError("Your message must be at least 1 character long.");
+			return;
+		}
 		setMessage(input.value);
+		setMessageError("");
 	}
 	return (
 		<>
@@ -27,12 +37,17 @@ export default function Contact(
 			<button onClick={() => alert(`My email address is ${props.email}`)}>
 				Show email address
 			</button>
-			<form onSubmit={props.onSendMessageFormSubmitted}>
+			<form onSubmit={(event) =>{
+				props.onSendMessageFormSubmitted(event);
+				setName("");
+				setMessage("");
+			}}>
 				<label htmlFor="contact-name">Your name (required)</label>
 				<input
 					type="text"
 					name="contact-name"
 					id="contact-name"
+					value={name}
 					onChange={updateName}
 					required
 				/>
@@ -41,12 +56,13 @@ export default function Contact(
 				<textarea
 					name="contact-message"
 					id="contact-message"
+					value={message}
 					onChange={updateMessage}
 					rows={20}
 					required
 				></textarea>
 				{messageError !== "" ? <p className="error-message">{messageError}</p> : ""}
-				<input type="submit" value="Send message" />
+				<input type="submit" value="Send message"/>
 				<h3>Sent data:</h3>
 				<pre></pre>
 			</form>
