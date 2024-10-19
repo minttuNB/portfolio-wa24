423662ca2848c05b43b07f524bb9546eda65546b
Minttu <cerealley@gmail.com>
2024-10-19 22:53:22 +0200

    Separated project fetching into read and readOne

--- frontend/src/features/projects/services/api.ts
+++ frontend/src/features/projects/services/api.ts
@@ -11,19 +11,17 @@ async function create(project: Partial<ProjectProps>) {
 		body: JSON.stringify(project),
 	});
 }
-async function read(id?: ReturnType<typeof crypto.randomUUID>) {
-	if (id) {
-		const data = await fetch(new URL(`${config.apiAddress}:${config.apiPort}/api/projects/${id}`)).then((res) =>
-			res.json()
-		);
-		return projectSchema.parse(data);
-	} else {
-		const data = await fetch(new URL(`${config.apiAddress}:${config.apiPort}/api/projects`)).then((res) =>
-			res.json()
-		);
-		console.log(projectsSchema.safeParse(data));
-		return projectsSchema.parse(data);
-	}
+async function read() {
+	const data: ProjectProps[] = await fetch(new URL(`${config.apiAddress}:${config.apiPort}/api/projects`)).then(
+		(res) => res.json()
+	);
+	return projectsSchema.parse(data);
+}
+async function readOne(id: ReturnType<typeof crypto.randomUUID>) {
+	const data: ProjectProps = await fetch(new URL(`${config.apiAddress}:${config.apiPort}/api/projects/${id}`)).then(
+		(res) => res.json()
+	);
+	return projectSchema.parse(data);
 }
 async function remove(id: ReturnType<typeof crypto.randomUUID>) {
 	return fetch(new URL(`${config.apiAddress}:${config.apiPort}/api/projects/${id}`), {
