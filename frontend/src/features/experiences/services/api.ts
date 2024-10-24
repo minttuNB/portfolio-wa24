58a194d05b1b25502e5c3a13859edce6eceab78a
Minttu <cerealley@gmail.com>
2024-10-24 21:29:03 +0200

    Split method into read and readOne (caused unclear return type errors)

--- frontend/src/features/experiences/services/api.ts
+++ frontend/src/features/experiences/services/api.ts
@@ -1,12 +1,19 @@
 import config from "../../../config";
 import { UUID } from "../../../types";
+import { validateExperience, validateExperiences } from "../lib/validate";
+import { ExperienceProps } from "../types";
 
-async function read(id?: UUID) {
-	if (id) {
-		return fetch(new URL(`${config.api.url}api/experiences/${id}`)).then((res) => res.json());
-	} else {
-		return fetch(new URL(`${config.api.url}api/experiences`)).then((res) => res.json());
-	}
+async function read() {
+	const data: ExperienceProps[] = await fetch(new URL(`${config.api.url}api/experiences`)).then((res) => res.json());
+	const validatedData = validateExperiences(data);
+	return validatedData;
+}
+async function readOne(id: UUID) {
+	const data: ExperienceProps = await fetch(new URL(`${config.api.url}api/experiences/${id}`)).then((res) =>
+		res.json()
+	);
+	const validatedData = validateExperience(data);
+	return validatedData;
 }
 export default {
 	read,
