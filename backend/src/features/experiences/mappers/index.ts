622fa69c3be9482fe9c9c782ad94079ae8f6bcc5
Minttu <cerealley@gmail.com>
2024-10-27 22:37:54 +0100

    create mappers between experience types

---
+++ backend/src/features/experiences/mappers/index.ts
@@ -0,0 +1,26 @@
+import { DbExperience, Experience } from "../types";
+
+export const fromDb = (experience: DbExperience) => {
+	return {
+		id: experience.id,
+		name: experience.name,
+		description: experience.description ?? null,
+		fromDate: new Date(experience.from_date),
+		toDate: experience.to_date ? new Date(experience.to_date) : null,
+		createdAt: new Date(experience.created_at),
+		updatedAt: experience.updated_at ? new Date(experience.updated_at) : null,
+		published: experience.published,
+	};
+};
+export const toDb = (experience: Experience) => {
+	return {
+		id: experience.id,
+		name: experience.name,
+		description: experience.description ?? "",
+		from_date: experience.fromDate.toISOString(),
+		to_date: experience.toDate ? experience.toDate?.toISOString() : null,
+		created_at: experience.createdAt.toISOString(),
+		updated_at: experience.updatedAt ? experience.updatedAt?.toISOString() : null,
+		published: experience.published,
+	};
+};
