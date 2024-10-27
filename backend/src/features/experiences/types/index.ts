68d759443bfc15a52499ddfbd9327975b65ed58c
Minttu <cerealley@gmail.com>
2024-10-27 22:37:43 +0100

    expand experience type; add dbexperience type

--- backend/src/features/experiences/types/index.ts
+++ backend/src/features/experiences/types/index.ts
@@ -6,6 +6,18 @@ type Experience = {
 	description?: string;
 	fromDate: Date;
 	toDate?: Date;
+	createdAt: Date;
+	updatedAt?: Date;
 	published: boolean;
 };
-export type { Experience };
+type DbExperience = {
+	id: string;
+	name: string;
+	description?: string;
+	from_date: string;
+	to_date?: string;
+	created_at: string;
+	updated_at?: string;
+	published: boolean;
+};
+export type { Experience, DbExperience };
