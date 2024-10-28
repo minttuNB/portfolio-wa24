e145ada638fcbd1436c5aea1d1615650a6b78fdc
Minttu <cerealley@gmail.com>
2024-10-28 14:21:57 +0100

    finish seed function with projects

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,7 +1,9 @@
 import { readFile } from "fs/promises";
 import prisma from "../src/db/client";
 import { toDb as experienceToDb } from "../src/features/experiences/mappers";
+import { toDb as projectToDb } from "../src/features/projects/mappers";
 import { experiencesSchema } from "../src/features/experiences/lib/validate";
+import { projectsSchema } from "../src/features/projects/lib/validate";
 
 const createExperiences = async () => {
 	const experiences = experiencesSchema.parse(JSON.parse(await readFile("data/experiences.json", "utf-8")));
@@ -15,9 +17,23 @@ const createExperiences = async () => {
 		})
 	);
 };
+const createProjects = async () => {
+	const projects = projectsSchema.parse(JSON.parse(await readFile("data/projects.json", "utf-8")));
+	await Promise.all(
+		projects.map(async (project) => {
+			console.log(projectToDb(project));
+			await prisma.project.create({
+				data: {
+					...projectToDb(project),
+				},
+			});
+		})
+	);
+};
 async function main() {
 	console.log(`Seeding is starting.`);
 	await createExperiences();
+	await createProjects();
 	console.log(`Seeding finished.`);
 }
 
