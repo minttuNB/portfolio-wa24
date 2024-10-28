50111e542c48026f6adf02be5d1d9237dfa57f3c
Minttu <cerealley@gmail.com>
2024-10-28 19:09:23 +0100

    Project service with setting created at date and validating incoming data

---
+++ backend/src/features/projects/service/index.ts
@@ -0,0 +1,91 @@
+import { UUID } from "crypto";
+import { projectSchema, validateUUID } from "../lib/validate";
+import { ProjectRepository } from "../repository";
+import { Project } from "../types";
+import { Result } from "../../../types";
+export interface ProjectService {
+	create: (data: Project) => Promise<Result<Project>>;
+	list: () => Promise<Result<Project[]>>;
+	getById: (id: UUID) => Promise<Result<Project>>;
+	update: (id: UUID, data: Partial<Project>) => Promise<Result<Project>>;
+	remove: (id: UUID) => Promise<Result<Project>>;
+	publish: (id: UUID) => Promise<Result<Project>>;
+	unpublish: (id: UUID) => Promise<Result<Project>>;
+}
+export const createProjectService = (projectRepository: ProjectRepository) => {
+	return {
+		create: async (data: Project) => {
+			try {
+				data = projectSchema.parse({ ...data, createdAt: new Date() });
+				return projectRepository.create(data);
+			} catch (error) {
+				const response: Result<Project> = {
+					success: false,
+					message: "Error creating the project: invalid project data",
+				};
+				return response;
+			}
+		},
+		list: projectRepository.list,
+		getById: async (id: UUID) => {
+			try {
+				id = validateUUID(id);
+				return projectRepository.getById(id);
+			} catch (error) {
+				const response: Result<Project> = {
+					success: false,
+					message: "Error fetching the project: invalid project id",
+				};
+				return response;
+			}
+		},
+		update: async (id: UUID, data: Partial<Project>) => {
+			try {
+				id = validateUUID(id);
+				return projectRepository.update(id, data);
+			} catch (error) {
+				const response: Result<Project> = {
+					success: false,
+					message: "Error updating the project: invalid project id",
+				};
+				return response;
+			}
+		},
+		remove: async (id: UUID) => {
+			try {
+				id = validateUUID(id);
+				return projectRepository.remove(id);
+			} catch (error) {
+				const response: Result<Project> = {
+					success: false,
+					message: "Error removing the project: invalid project id",
+				};
+				return response;
+			}
+		},
+		publish: async (id: UUID) => {
+			try {
+				id = validateUUID(id);
+				return projectRepository.publish(id);
+			} catch (error) {
+				const response: Result<Project> = {
+					success: false,
+					message: "Error publishing the project: invalid project id",
+				};
+				return response;
+			}
+		},
+		unpublish: async (id: UUID) => {
+			try {
+				id = validateUUID(id);
+				return projectRepository.unpublish(id);
+			} catch (error) {
+				const response: Result<Project> = {
+					success: false,
+					message: "Error unpublishing the project: invalid project id",
+				};
+				return response;
+			}
+		},
+	};
+};
